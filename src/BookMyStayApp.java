bc42c46d185f18e08d313067c03fc44c0a6b7b57
Prakh4r <prakhardhawan1218@gmail.com>
2026-03-13 10:22:24 +0530

    Base Application Setup

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,7 +1,5 @@
 class BookMyStayApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Book My Stay App");
-        System.out.println("Version 1.0");
-        System.out.println("System initialized successfully");
+
     }
 }
